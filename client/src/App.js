231fd80f8a9c2c08437c4ba4b43d8ddfb0a88746
Artem <shabarovartemy@yandex.ru>
2024-10-17 18:28:55 +0300

    Made page Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRouter />
+        <NavBar/>
+        <AppRouter />
     </BrowserRouter>
   );
 }
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import { Button, Card, Form, Row } from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                <Form className="d-flex flex-column align-items-center">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш пароль..."
+                    />
+                    <Row className="d-flex justify-content-between align-items-center mt-3 ps-3 pe-3">
+                        {isLogin ?
+                            <div>
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                            </div>
+                            :
+                            <div>
+                            Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>}
+                        <Button
+                            className="mt-3"
+                            variant={"outline-success"}
+                        >
+                            {isLogin ? 'Войти' : 'Регистрация'}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
