1c548e95921a2779efa71b37dfc5cab0280d8823
Artem <shabarovartemy@yandex.ru>
2024-10-17 15:39:09 +0300

    Add store

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Route, Routes} from 'react-router-dom'
 import {authRoutes, publicRoutes} from "../routes";
 import {SHOP_ROUTE} from "../utils/consts";
+import {Context} from "../index";
 
 const AppRouter = () => {
-    const isAuth = false;
+    const {user} = useContext(Context)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({ path, Component }) => (
+            {user.isAuth && authRoutes.map(({ path, Component }) => (
                 <Route key={path} path={path} element={<Component />} exact />
             ))}
             {publicRoutes.map(({ path, Component }) => (
---
+++ client/src/store/UserStore.js
@@ -0,0 +1,27 @@
+import {makeAutoObservable} from "mobx";
+
+export default class UserStore {
+    constructor() {
+        //нижнее подчеркивание, значит переменные нельзя изменять (соглашение)
+        this._isAuth = false
+        this._user = {}
+        //будет следить за изменениями переменных
+        //при их изменении будут перерендериватся
+        makeAutoObservable(this)
+    }
+//функции изменяющие состояния
+    setIsAuth(bool) {
+        this._isAuth = bool
+    }
+    setUser(user) {
+        this._user = user
+    }
+//одноименные гетеры (чтобы получать переменные из состояния)
+    get isAuth() {
+        return this._isAuth;
+    }
+
+    get user() {
+        return this._user;
+    }
+}
