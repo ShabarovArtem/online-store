71b693eefe054b07fdc98d3ee3c27bc3f085fba6
Artem <shabarovartemy@yandex.ru>
2024-10-18 22:01:07 +0300

    Made main page

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,15 +4,28 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             {id: 1, name: 'Холодильники'},
-            {id: 2, name: 'Смартфоны'}
+            {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Ноутбуки'},
+            {id: 4, name: 'Телевизоры'},
         ]
         this._brands = [
             {id: 1, name: 'Samsung'},
-            {id: 2, name: 'Apple'}
+            {id: 2, name: 'Apple'},
+            {id: 3, name: 'Lenovo'},
+            {id: 4, name: 'Asus'},
         ]
         this._devices = [
             {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 2, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 3, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 4, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 5, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 6, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 7, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
+            {id: 8, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/08/Iphone.jpg'},
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
     setTypes(types) {
