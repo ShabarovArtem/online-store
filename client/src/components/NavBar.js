a61bf9ec741b5c38dc5db581c225bf57cee27979
Artem <shabarovartemy@yandex.ru>
2024-10-23 17:16:03 +0300

    Add Admin panel

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
@@ -10,24 +10,41 @@ import Container from "react-bootstrap/Container";
 
 //observer - чтобы отслеживать изменения в режиме реального времени
 const NavBar = observer(() => {
-    const {user} = useContext(Context)
+    const { user } = useContext(Context);
+    const navigate = useNavigate(); // Замена useHistory на useNavigate
+
+    const logOut = () => {
+        user.setUser({});
+        user.setIsAuth(false);
+    };
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+                <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
-                    <Nav className="ml-auto" style={{color:'white'}}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className="ms-2">Войти</Button>
+                    <Nav className="ms-auto" style={{ color: 'white' }}>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(ADMIN_ROUTE)} // Используем navigate вместо history.push
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={logOut} // Упрощаем onClick
+                            className="ms-2"
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
-                    <Nav className="ml-auto" style={{color:'white'}}>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                    <Nav className="ms-auto" style={{ color: 'white' }}>
+                        <Button variant={"outline-light"} onClick={() => navigate(LOGIN_ROUTE)}>Авторизация</Button>
                     </Nav>
                 }
             </Container>
         </Navbar>
-
     );
 });
 
