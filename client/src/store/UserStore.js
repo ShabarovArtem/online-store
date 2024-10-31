5b86df1237b18d4413919a5489063b6c73945e5e
Artem <shabarovartemy@yandex.ru>
2024-10-31 23:37:49 +0300

    Change Auth page
    Init navigate

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -3,7 +3,7 @@ import {makeAutoObservable} from "mobx";
 export default class UserStore {
     constructor() {
         //нижнее подчеркивание, значит переменные нельзя изменять (соглашение)
-        this._isAuth = true
+        this._isAuth = false
         this._user = {}
         //будет следить за изменениями переменных
         //при их изменении будут перерендериватся
