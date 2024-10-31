7ea90c5bef7ed78b5192f7f70391679799134696
Artem <shabarovartemy@yandex.ru>
2024-10-31 22:02:30 +0300

    Fix Auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from "react-bootstrap/Container";
 import { Button, Card, Form, Row } from "react-bootstrap";
 import {NavLink, useLocation} from "react-router-dom";
 import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import {login, registration} from "../http/userAPI";
 
 const Auth = () => {
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const click = async () => {
+        if (isLogin) {
+            const response = await login()
+            console.log(response)
+
+        } else {
+            console.log("Base URL:", process.env.REACT_APP_API_URL);
+            const response = await registration(email, password)
+            console.log(response)
+        }
+    }
 
     return (
         <Container
@@ -19,10 +34,15 @@ const Auth = () => {
                     <Form.Control
                         className="mt-3"
                         placeholder="Введите ваш email..."
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <Form.Control
                         className="mt-3"
                         placeholder="Введите ваш пароль..."
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
                     />
                     <Row className="d-flex justify-content-between align-items-center mt-3 ps-3 pe-3">
                         {isLogin ?
@@ -36,6 +56,7 @@ const Auth = () => {
                         <Button
                             className="mt-3"
                             variant={"outline-success"}
+                            onClick={click}
                         >
                             {isLogin ? 'Войти' : 'Регистрация'}
                         </Button>
